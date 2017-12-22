5678019eceb8e485272111b19760c990e849e542
Rafaelfscomand <rafael5.r@hotmail.com>
2017-12-22 18:04:39 -0200

    update3

--- trocador/duplotubo/views.py
+++ trocador/duplotubo/views.py
@@ -1,34 +1,50 @@
 from django.shortcuts import render
 from trocador.duplotubo.forms import Calculo
-from django.http import HttpResponse  
-from trocador.duplotubo.metodo_kern import *;
+from trocador.duplotubo.metodo_kern import *
 from trocador.duplotubo.models import *
 from trocador.duplotubo.forms import *
-from django.db import connection
-import sqlite3
+from trocador.core.models import *
 from django.contrib.auth.decorators import login_required
 
+ 
 def propried_get(Table,temp,temp_w,fluido):
-    def trecho(Table,temp):
-        query1=Table.objects.filter(temperature__gte=temp)[0] 
-        query2=Table.objects.filter(temperature__lte=temp)[len(Table.objects.filter(temperature__lte=temp))-1] 
-        temp1=query1.temperature;temp2=query2.temperature
-        return query1,query2,temp1,temp2
-    for propriedade in fluido.keys():
-        try:
-            if propriedade=='Viscos_tw':
-                query1,query2,temp1,temp2=trecho(Agua,temp_w)
-                prop1=eval('query1.Viscos');prop2=eval('query2.Viscos')
-            else:
-                query1,query2,temp1,temp2=trecho(Agua,temp)
-                prop1=eval('query1.%s'%propriedade);prop2=eval('query2.%s'%propriedade)
-            fluido[propriedade]=(prop2-prop1)*(temp-temp1)/(temp2-temp1) + prop1 if prop2>prop1 else -(prop1-prop2)*(temp-temp1)/(temp2-temp1) + prop1
-        except: pass
-    fluido['Pr']=fluido['cp']*fluido['Viscos']/fluido['k']
-    return fluidos
+	def trecho(Table,temp):
+		query1=Table.objects.filter(temperature__gte=temp)[0] 
+		query2=Table.objects.filter(temperature__lte=temp)[len(Table.objects.filter(temperature__lte=temp))-1] 
+		temp1=query1.temperature;temp2=query2.temperature
+		return query1,query2,temp1,temp2
+	for propriedade in fluido.keys():
+		try:
+			if propriedade=='Viscos_tw':
+				query1,query2,temp1,temp2=trecho(Agua,temp_w)
+				prop1=eval('query1.Viscos');prop2=eval('query2.Viscos')
+			else:
+				query1,query2,temp1,temp2=trecho(Agua,temp)
+				prop1=eval('query1.%s'%propriedade);prop2=eval('query2.%s'%propriedade)
+			fluido[propriedade]=(prop2-prop1)*(temp-temp1)/(temp2-temp1) + prop1 if prop2>prop1 else -(prop1-prop2)*(temp-temp1)/(temp2-temp1) + prop1
+		except: pass
+	fluido['Pr']=fluido['cp']*fluido['Viscos']/fluido['k']
+	return fluidos
+
+def atribut(formulario,dicionario,identificador=''):
+	for dado in formulario.cleaned_data.keys():
+		if dado.replace(identificador,'') in dicionario.keys():
+			dicionario[dado.replace(identificador,'')]=formulario.cleaned_data[dado]
+	if 'Annulus' in dicionario.keys():
+		dicionario['Diam_int']=dicionario['Diam_int']/1000.0
+		dicionario['Diam_ext']=dicionario['Diam_ext']/1000.0
+		dicionario['Annulus']=0 if identificador is 'Interno' else 1
+	return	dicionario
 
 @login_required
 def calculo_duplotubo(request):
+	fluidoInterno={'Vazao':0,'T_entr':0,'T_said':0,'cp':0,'k':0,'Pr':0,'Viscos':0,
+	         'Densidade':0,'Diam_ext':0,'Diam_int':0,'Annulus':0,'Liquido':0,'Viscos_tw':0}
+	fluidoExterno={'Vazao':0,'T_entr':0,'T_said':0,'cp':0,'k':0,'Pr':0,'Viscos':0,
+	         'Densidade':0,'Diam_ext':0,'Diam_int':0,'Annulus':1,'Liquido':0,'Viscos_tw':0}
+	temp_m1=(form.cleaned_data['T_entr1']+form.cleaned_data['T_said1'])/2
+	temp_m2=(form.cleaned_data['T_entr2']+form.cleaned_data['T_said2'])/2
+	temp_w=(form.cleaned_data['T_entr1']+form.cleaned_data['T_said1']+form.cleaned_data['T_entr2']+form.cleaned_data['T_said2'])/4
 	#import pdb; pdb.set_trace()
 	exibition = Resultado()
 	template_name = 'duplotubo/calculo_duplotubo.html'
@@ -36,204 +52,45 @@ def calculo_duplotubo(request):
 
 	if form.is_valid():
 		if form.cleaned_data['nome_fluido1']=='manual': 
-			fiul1={'Vazao':form.cleaned_data['Vazao1'],
-			'T_entr':form.cleaned_data['T_entr1'],
-			'T_said':form.cleaned_data['T_said1'],
-			'k':form.cleaned_data['k1'],
-			'cp':form.cleaned_data['cp1'],
-			'Pr':form.cleaned_data['Pr1'],
-			'Viscos':form.cleaned_data['Viscosidade1'],
-			'Densidade':form.cleaned_data['Densidade1'],
-			'Diam_ext':form.cleaned_data['Diam_ext1']/1000.0,
-			'Diam_int':form.cleaned_data['Diam_int1']/1000.0,
-			'Annulus':0,
-			'Liquido':1,
-			'Viscos_tw':form.cleaned_data['Viscos_tw1']}
+			fiul1=atribut(form.cleaned_data,fluidoInterno,identificador="Interno")
 
 		if form.cleaned_data['nome_fluido2']=='manual': 
-			fiul2={'Vazao':form.cleaned_data['Vazao2'],
-			'T_entr':form.cleaned_data['T_entr2'],
-			'T_said':form.cleaned_data['T_said2'],
-			'k':form.cleaned_data['k2'],
-			'cp':form.cleaned_data['cp2'],
-			'Pr':form.cleaned_data['Pr2'],
-			'Viscos':form.cleaned_data['Viscosidade2'],
-			'Densidade':form.cleaned_data['Densidade2'],
-			'Diam_ext':form.cleaned_data['Diam_ext2']/1000.0,
-			'Diam_int':form.cleaned_data['Diam_int2']/1000.0,
-			'Annulus':1,
-			'Liquido':1,
-			'Viscos_tw':form.cleaned_data['Viscos_tw2']}
-		elif form.cleaned_data['nome_fluido2']!='manual' or form.cleaned_data['nome_fluido1']!='manual':
+			fiul2=atribut(form.cleaned_data,fluidoExterno,identificador='Externo')
 
-			cur=sqlite3.connect('db.sqlite3').cursor()
-			dic1={'k':0,'cp':0,'Pr':0,'Viscos':0,'Viscos_tw':0}
-			dic2={'k':0,'cp':0,'Pr':0,'Viscos':0,'Viscos_tw':0}
-			temp_m1=(form.cleaned_data['T_entr1']+form.cleaned_data['T_said1'])/2
-			temp_m2=(form.cleaned_data['T_entr2']+form.cleaned_data['T_said2'])/2
-			temp_w=(form.cleaned_data['T_entr1']+form.cleaned_data['T_said1']+form.cleaned_data['T_entr2']+form.cleaned_data['T_said2'])/4
-			propried_get(tuple([form.cleaned_data['nome_fluido1']]),temp_m1,temp_w,dic1)
-			propried_get(tuple([form.cleaned_data['nome_fluido2']]),temp_m2,temp_w,dic2)
+		elif form.cleaned_data['nome_fluido2']!='manual' or form.cleaned_data['nome_fluido1']!='manual':
+			propried_get(eval(form.cleaned_data['nome_fluido1']),temp_m1,temp_w,fluidoInterno)
+			propried_get(eval(form.cleaned_data['nome_fluido2']),temp_m2,temp_w,fluidoExterno)
 
 	context={'form': form}
 	if form.is_valid():
 		if form.cleaned_data['nome_fluido1']=='manual' and form.cleaned_data['nome_fluido2']!='manual':
 			fluido1, fluido2, material = yut(fiul1,
-
-				{'Vazao':form.cleaned_data['Vazao2'],
-				'T_entr':form.cleaned_data['T_entr2'],
-				'T_said':form.cleaned_data['T_said2'],
-				'k':dic2['k'] if bool(dic2['k']) else form.cleaned_data['k2'],
-				'cp':dic2['cp'] if bool(dic2['cp']) else form.cleaned_data['cp2'],
-				'Pr':dic2['Pr'] if bool(dic2['Pr']) else form.cleaned_data['Pr2'],
-				'Viscos':dic2['Viscos'] if bool(dic2['Viscos']) else form.cleaned_data['Viscosidade2'],
-				'Densidade':dic1['Densidade'] if bool(dic1['Densidade']) else form.cleaned_data['Densidade2'],
-				'Diam_ext':form.cleaned_data['Diam_ext2']/1000.0,
-				'Diam_int':form.cleaned_data['Diam_int2']/1000.0,
-				'Annulus':1,'Liquido':1,
-				'Viscos_tw':dic2['Viscos_tw'] if bool(dic2['Viscos_tw']) else form.cleaned_data['Viscos_tw2']},
-
-				{'K':form.cleaned_data['K'],
-				'L':form.cleaned_data['L'],
-				'R_fi':form.cleaned_data['R_fi'],
-				'R_fo':form.cleaned_data['R_fo'],
-				'Calor_cnste':form.cleaned_data['Calor_const'],
-				'Contracorrente':form.cleaned_data['Contracorrente'],
-				'Efic_bomb1':form.cleaned_data['ef_b1']/100.0,
-				'Efic_bomb2':form.cleaned_data['ef_b2']/100.0,
-				'Num_tubs':form.cleaned_data['Num_tubs'],
-				'Alet_per_tube':form.cleaned_data['Alet_num'],
-				'Alet_alt':form.cleaned_data['Alet_alt']/1000.0,
-				'Alet_K':form.cleaned_data['Alet_k'],
-				'Alet_espes':form.cleaned_data['Alet_esp']/1000.0,
-				'Alet_type':form.cleaned_data['Alet_type'],
-				'Tubo_aletado':form.cleaned_data['Alet_tubes'],
-				'Multi_tube':form.cleaned_data['Multi_tubes'],
-				'arranj_ser_paral':form.cleaned_data['arranj_ser_paral'],
-				'T_I_Paralelo':form.cleaned_data['T_I_Paralelo'],
-				'R_A_Paralelo':form.cleaned_data['R_A_Paralelo'],
-				'Num_ramos':form.cleaned_data['Num_ramos']})
+				propried_get(eval(form.cleaned_data['nome_fluido2']),temp_m2,temp_w,fluidoExterno),
+				atribut(form.cleaned_data,material))
 
 		if form.cleaned_data['nome_fluido2']=='manual' and form.cleaned_data['nome_fluido1']!='manual':
-			fluido1, fluido2, material = yut(	
-				{'Vazao':form.cleaned_data['Vazao2'],
-				'T_entr':form.cleaned_data['T_entr2'],
-				'T_said':form.cleaned_data['T_said2'],
-				'k':dic2['k'] if bool(dic2['k']) else form.cleaned_data['k2'],
-				'cp':dic2['cp'] if bool(dic2['cp']) else form.cleaned_data['cp2'],
-				'Pr':dic2['Pr'] if bool(dic2['Pr']) else form.cleaned_data['Pr2'],
-				'Viscos':dic2['Viscos'] if bool(dic2['Viscos']) else form.cleaned_data['Viscosidade2'],
-				'Densidade':dic1['Densidade'] if bool(dic1['Densidade']) else form.cleaned_data['Densidade2'],
-				'Diam_ext':form.cleaned_data['Diam_ext2']/1000.0,
-				'Diam_int':form.cleaned_data['Diam_int2']/1000.0,
-				'Annulus':1,
-				'Liquido':1,
-				'Viscos_tw':dic2['Viscos_tw'] if bool(dic2['Viscos_tw']) else form.cleaned_data['Viscos_tw2']},
-
+			fluido1, fluido2, material = yut(
+				propried_get(eval(form.cleaned_data['nome_fluido1']),temp_m1,temp_w,fluidoInterno),
 				fiul2,
-
-				{'K':form.cleaned_data['K'],
-				'L':form.cleaned_data['L'],
-				'R_fi':form.cleaned_data['R_fi'],
-				'R_fo':form.cleaned_data['R_fo'],
-				'Calor_cnste':form.cleaned_data['Calor_const'],
-				'Contracorrente':form.cleaned_data['Contracorrente'],
-				'Efic_bomb1':form.cleaned_data['ef_b1']/100.0,
-				'Efic_bomb2':form.cleaned_data['ef_b2']/100.0,
-				'Num_tubs':form.cleaned_data['Num_tubs'],
-				'Alet_per_tube':form.cleaned_data['Alet_num'],
-				'Alet_alt':form.cleaned_data['Alet_alt']/1000.0,
-				'Alet_K':form.cleaned_data['Alet_k'],
-				'Alet_espes':form.cleaned_data['Alet_esp']/1000.0,
-				'Alet_type':form.cleaned_data['Alet_type'],
-				'Tubo_aletado':form.cleaned_data['Alet_tubes'],
-				'Multi_tube':form.cleaned_data['Multi_tubes'],
-				'arranj_ser_paral':form.cleaned_data['arranj_ser_paral'],
-				'T_I_Paralelo':form.cleaned_data['T_I_Paralelo'],
-				'R_A_Paralelo':form.cleaned_data['R_A_Paralelo'],
-				'Num_ramos':form.cleaned_data['Num_ramos']})
+				atribut(form.cleaned_data,material))
 
 		elif form.cleaned_data['nome_fluido1']=='manual' and form.cleaned_data['nome_fluido2']=='manual':
-			fluido1, fluido2, material = yut(fiul1,fiul2,
-				{'K':form.cleaned_data['K'],
-				'L':form.cleaned_data['L'],
-				'R_fi':form.cleaned_data['R_fi'],
-				'R_fo':form.cleaned_data['R_fo'],
-				'Calor_cnste':form.cleaned_data['Calor_const'],
-				'Contracorrente':form.cleaned_data['Contracorrente'],
-				'Efic_bomb1':form.cleaned_data['ef_b1']/100.0,
-				'Efic_bomb2':form.cleaned_data['ef_b2']/100.0,
-				'Num_tubs':form.cleaned_data['Num_tubs'],
-				'Alet_per_tube':form.cleaned_data['Alet_num'],
-				'Alet_alt':form.cleaned_data['Alet_alt']/1000.0,
-				'Alet_K':form.cleaned_data['Alet_k'],
-				'Alet_espes':form.cleaned_data['Alet_esp']/1000.0,
-				'Alet_type':form.cleaned_data['Alet_type'],
-				'Tubo_aletado':form.cleaned_data['Alet_tubes'],
-				'Multi_tube':form.cleaned_data['Multi_tubes'],
-				'arranj_ser_paral':form.cleaned_data['arranj_ser_paral'],
-				'T_I_Paralelo':form.cleaned_data['T_I_Paralelo'],
-				'R_A_Paralelo':form.cleaned_data['R_A_Paralelo'],
-				'Num_ramos':form.cleaned_data['Num_ramos']})
-
-
+			fluido1, fluido2, material = yut(fiul1,fiul2,atribut(form.cleaned_data,material))
 		else:
+
 			fluido1, fluido2, material = yut(
 				# Fluido1
-				{'Vazao':form.cleaned_data['Vazao1'],
-				'T_entr':form.cleaned_data['T_entr1'],
-				'T_said':form.cleaned_data['T_said1'],
-				'k':dic1['k'] if bool(dic1['k']) else form.cleaned_data['k1'],
-				'cp':dic1['cp'] if bool(dic1['cp']) else form.cleaned_data['cp1'],
-				'Pr':dic1['Pr'] if bool(dic1['Pr']) else form.cleaned_data['Pr1'],
-				'Viscos':dic1['Viscos'] if bool(dic1['Viscos']) else form.cleaned_data['Viscosidade1'],
-				'Densidade':dic1['Densidade'] if bool(dic1['Densidade']) else form.cleaned_data['Densidade1'],
-				'Diam_ext':form.cleaned_data['Diam_ext1']/1000.0,
-				'Diam_int':form.cleaned_data['Diam_int1']/1000.0,
-				'Annulus':0,
-				'Liquido':1,
-				'Viscos_tw':dic1['Viscos_tw'] if bool(dic1['Viscos_tw']) else form.cleaned_data['Viscos_tw1']},
+				propried_get(eval(form.cleaned_data['nome_fluido1']),temp_m1,temp_w,fluidoInterno),
 				# Fluido2
-				{'Vazao':form.cleaned_data['Vazao2'],
-				'T_entr':form.cleaned_data['T_entr2'],
-				'T_said':form.cleaned_data['T_said2'],
-				'k':dic2['k'] if bool(dic2['k']) else form.cleaned_data['k2'],
-				'cp':dic2['cp'] if bool(dic2['cp']) else form.cleaned_data['cp2'],
-				'Pr':dic2['Pr'] if bool(dic2['Pr']) else form.cleaned_data['Pr2'],
-				'Viscos':dic2['Viscos'] if bool(dic2['Viscos']) else form.cleaned_data['Viscosidade2'],
-				'Densidade':dic1['Densidade'] if bool(dic1['Densidade']) else form.cleaned_data['Densidade2'],
-				'Diam_ext':form.cleaned_data['Diam_ext2']/1000.0,
-				'Diam_int':form.cleaned_data['Diam_int2']/1000.0,
-				'Annulus':1,
-				'Liquido':1,
-				'Viscos_tw':dic2['Viscos_tw'] if bool(dic2['Viscos_tw']) else form.cleaned_data['Viscos_tw2']},
+				propried_get(eval(form.cleaned_data['nome_fluido2']),temp_m2,temp_w,fluidoExterno),
 				# Material
-				{'K':form.cleaned_data['K'],
-				'L':form.cleaned_data['L'],
-				'R_fi':form.cleaned_data['R_fi'],
-				'R_fo':form.cleaned_data['R_fo'],
-				'Calor_cnste':form.cleaned_data['Calor_const'],
-				'Contracorrente':form.cleaned_data['Contracorrente'],
-				'Efic_bomb1':form.cleaned_data['ef_b1']/100.0,
-				'Efic_bomb2':form.cleaned_data['ef_b2']/100.0,
-				'Num_tubs':form.cleaned_data['Num_tubs'],
-				'Alet_per_tube':form.cleaned_data['Alet_num'],
-				'Alet_alt':form.cleaned_data['Alet_alt']/1000.0,
-				'Alet_K':form.cleaned_data['Alet_k'],
-				'Alet_espes':form.cleaned_data['Alet_esp']/1000.0,
-				'Alet_type':form.cleaned_data['Alet_type'],
-				'Tubo_aletado':form.cleaned_data['Alet_tubes'],
-				'Multi_tube':form.cleaned_data['Multi_tubes'],
-				'arranj_ser_paral':form.cleaned_data['arranj_ser_paral'],
-				'T_I_Paralelo':form.cleaned_data['T_I_Paralelo'],
-				'R_A_Paralelo':form.cleaned_data['R_A_Paralelo'],
-				'Num_ramos':form.cleaned_data['Num_ramos']})
+				atribut(form.cleaned_data,material))
 
-			res = Resultado()
-			res.result_fl = fluido1
-			res.result_pres = fluido2
-			res.result_geral = material
-			res.save()
+		res = Resultado()
+		res.result_fl = fluido1
+		res.result_pres = fluido2
+		res.result_geral = material
+		res.save()
 
 		context = {'form': form,
 					'Vazao1':fluido1['Vazao'][0],
